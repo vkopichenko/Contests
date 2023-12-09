dfac26dbf1e441d27c649b0c3286aed383b91013
Vadzim Kapichenka <v.kapichenka@godeltech.com>
2023-12-09 12:13:37 +0100

    Advent of Code 2023 Day 9: Mirage Maintenance

---
+++ adventofcode/src/2023/y23day9part1.kt
@@ -0,0 +1,9 @@
+
+fun main() {
+    fun List<Int>.extrapolate(): Int =
+        if (isEmpty()) 0
+        else last() + asSequence().windowed(2).map { (a, b) -> b - a }.toList().extrapolate()
+    generateSequence { readlnOrNull()?.split(' ')?.map { it.toInt() } }
+        .map { it.extrapolate() }
+        .sum().let(::println)
+}
---
+++ adventofcode/src/2023/y23day9part2.kt
@@ -0,0 +1,9 @@
+
+fun main() {
+    fun List<Int>.extrapolate(): Int =
+        if (isEmpty()) 0
+        else first() - windowed(2).map { (a, b) -> b - a }.extrapolate()
+    generateSequence { readlnOrNull()?.split(' ')?.map { it.toInt() } }
+        .map { it.extrapolate() }
+        .sum().let(::println)
+}
