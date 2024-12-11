d8a70ffd71eef33ae4a08ccc1e6a21b5b1df5e86
Vadzim Kapichenka <v.kapichenka@godeltech.com>
2024-12-11 22:32:58 +0100

    Advent of Code 2024 Day 10: Hoof It - obsessive refactoring to reuse abstracted bfs between parts

--- adventofcode/src/2024/y24day10.kt
+++ adventofcode/src/2024/y24day10.kt
@@ -1,73 +1,55 @@
-package y24day10;
-
-import y24day10.Direction.*
-import java.util.*
 import kotlin.time.measureTimedValue
 
-enum class Direction {
-    left, up, right, down
-}
-data class Pos(val i: Int, val j: Int)
-fun Pos.move(dir: Direction) = when (dir) {
-    up -> Pos(i - 1, j)
-    down -> Pos(i + 1, j)
-    left -> Pos(i, j - 1)
-    right -> Pos(i, j + 1)
-}
-fun List<IntArray>.at(pos: Pos) = getOrNull(pos.i)?.getOrNull(pos.j)
-fun Pos.directions() = entries.asSequence().map(::move)
-
 fun main() {
+    data class Pos(val i: Int, val j: Int)
+    fun Pos.adjacentPositions() = sequenceOf(Pos(i - 1, j), Pos(i + 1, j), Pos(i, j - 1), Pos(i, j + 1))
+    fun List<IntArray>.at(pos: Pos) = getOrNull(pos.i)?.getOrNull(pos.j)
+
     val map = generateSequence { readlnOrNull()?.map { it.digitToInt() }?.toIntArray() }.toList()
 
-    measureTimedValue { // part 1
+    fun <S> bfs(
+        initialVisitState: () -> S,
+        shouldVisit: (S, Pos, Pos) -> Boolean,
+        trailheadScore: (S, Pos) -> Int
+    ): Int =
         map.indices.sumOf { i ->
             map[i].indices.asSequence().filter { j ->
                 map[i][j] == 0
             }.sumOf { j ->
                 var trailheadScores = 0
-                val visited = Array(map.size) { BooleanArray(map[0].size) { false } }
-                visited[i][j] = true
+                val visitCache = initialVisitState()
                 val queue = ArrayDeque<Pos>()
-                queue += Pos(i, j)
+                queue += Pos(i, j).also { shouldVisit(visitCache, it, it) }
                 while (true) {
-                    val pos = queue.poll() ?: break
+                    val pos = queue.removeFirstOrNull() ?: break
                     val depth = map.at(pos)!!
-                    if (depth == 9) ++trailheadScores
-                    else for (next in pos.directions()) {
-                        if (map.at(next) != depth + 1 || visited[next.i][next.j]) continue
-                        visited[next.i][next.j] = true
-                        queue += next
+                    if (depth == 9) trailheadScores += trailheadScore(visitCache, pos)
+                    else for (next in pos.adjacentPositions()) {
+                        if (map.at(next) == depth + 1 && shouldVisit(visitCache, next, pos)) queue += next
                     }
                 }
                 trailheadScores
             }
         }
+
+    measureTimedValue { // part 1
+        bfs(
+            initialVisitState = { Array(map.size) { BooleanArray(map[0].size) { false } } },
+            shouldVisit = { visited, pos, _ ->
+                pos.run { visited[i][j].not().also { if (it) visited[i][j] = true } }
+            },
+            trailheadScore = { _, _ -> 1 },
+        )
     }.also(::println)
 
     measureTimedValue { // part 2
-        map.indices.sumOf { i ->
-            map[i].indices.asSequence().filter { j ->
-                map[i][j] == 0
-            }.sumOf { j ->
-                var trailheadScores = 0
-                val visitCounts = Array(map.size) { IntArray(map[0].size) { 0 } }
-                visitCounts[i][j] = 1
-                val queue = ArrayDeque<Pos>()
-                queue += Pos(i, j)
-                while (true) {
-                    val pos = queue.poll() ?: break
-                    val depth = map.at(pos)!!
-                    if (depth == 9) trailheadScores += visitCounts[pos.i][pos.j]
-                    else for (next in pos.directions()) {
-                        if (map.at(next) != depth + 1) continue
-                        val visited = visitCounts[next.i][next.j] > 0
-                        visitCounts[next.i][next.j] += visitCounts[pos.i][pos.j]
-                        if (!visited) queue += next
-                    }
-                }
-                trailheadScores
-            }
-        }
+        bfs(
+            initialVisitState = { Array(map.size) { IntArray(map[0].size) { 0 } } },
+            shouldVisit = { visitCounts, pos, from ->
+                if (pos == from) false.also { visitCounts[pos.i][pos.j] = 1 }
+                else (visitCounts[pos.i][pos.j] == 0).also { visitCounts[pos.i][pos.j] += visitCounts[from.i][from.j] }
+            },
+            trailheadScore = { visitCounts, pos -> visitCounts[pos.i][pos.j] },
+        )
     }.also(::println)
 }
